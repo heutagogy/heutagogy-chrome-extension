cae995af9f173fca8694308d159dd29a9c910997
Alexey Shmalko <rasen.dubi@gmail.com>
2017-01-29 16:23:06 +0200

    Request more bookmarks from the server
    Problem: I have more than 20 bookmarks, and extension behaviour is
    unpredictable.
    
    Solution: Request more bookmarks in one request. This is a workaround,
    but should for now.

--- app/utils/loader.js
+++ app/utils/loader.js
@@ -3,7 +3,7 @@ import article from '../schemas/article';
 
 export const loadEntities = (store) => {
   load({
-    href: '/bookmarks',
+    href: '/bookmarks?per_page=200',
     schema: article,
   })(store.dispatch);
 };
