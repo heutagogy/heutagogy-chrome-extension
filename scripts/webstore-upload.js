5ca56385d577a84bf45da44384f247e5e00673d3
Alexey Shmalko <rasen.dubi@gmail.com>
2017-02-01 00:41:49 +0200

    Fix linter

--- scripts/webstore-upload.js
+++ scripts/webstore-upload.js
@@ -1,29 +1,32 @@
-const fs = require('fs');
-var AdmZip = require('adm-zip');
+const fs = require('fs'); //eslint-disable-line
+const AdmZip = require('adm-zip'); //eslint-disable-line
+const chromeWebstoreUpload = require('chrome-webstore-upload'); //eslint-disable-line
 
-console.log("Creating zip archive...");
-var zip = new AdmZip();
+console.log('Creating zip archive...');
+const zip = new AdmZip();
+
 zip.addLocalFolder('./build', 'heutagogy');
 zip.writeZip('heutagogy.zip');
 
-const webStore = require('chrome-webstore-upload')({
+const webStore = chromeWebstoreUpload({
   extensionId: process.env.WEBSTORE_EXTENSION_ID,
   clientId: process.env.WEBSTORE_CLIENT_ID,
   clientSecret: process.env.WEBSTORE_CLIENT_SECRET,
-  refreshToken: process.env.WEBSTORE_REFRESH_TOKEN
+  refreshToken: process.env.WEBSTORE_REFRESH_TOKEN,
 });
 
-console.log("Fetching token...");
-webStore.fetchToken().then(token => {
+console.log('Fetching token...');
+webStore.fetchToken().then((token) => {
   const zipFile = fs.createReadStream('./heutagogy.zip');
-  console.log("Uploading...");
-  webStore.uploadExisting(zipFile, token).then(res => {
+
+  console.log('Uploading...');
+  webStore.uploadExisting(zipFile, token).then((res) => {
     console.log(res);
 
     if (res.uploadState === 'SUCCESS') {
-      console.log("Publishing...");
-      webStore.publish().then(res => {
-        console.log(res);
+      console.log('Publishing...');
+      webStore.publish().then((publishResult) => {
+        console.log(publishResult);
       });
     }
   });
